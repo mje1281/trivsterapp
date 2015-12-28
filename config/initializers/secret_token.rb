2a03f7d5419295912acb2cf145e1518a67eb3537
mje1281 <mje1281@yahoo.com>
2015-12-28 21:59:55 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trivster::Application.config.secret_key_base = '9f98f019e5fa4e895695e513e1f8d5ac5a199a0a1dcddd0d6911deb1faaf7913a2c4661d8c95b368af7edd1fd84a53944fc11fd451ac5f6ef6bde5fefd0ed0d4'
